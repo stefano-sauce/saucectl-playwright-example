54d07daabf8218ccd401054a8bf5065e938a5a8e
mitesting <stefano.loi@saucelabs.com>
2025-08-20 14:14:19 +0100

    tagging

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,7 +1,8 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('homepage has Playwright in title and get started link linking to the intro page', async ({ page }, testInfo) => {
+// This test is now tagged for both 'sanity' and 'regression' suites.
+test('homepage has Playwright in title and get started link linking to the intro page @sanity @regression', async ({ page }, testInfo) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
@@ -21,4 +22,13 @@ test('homepage has Playwright in title and get started link linking to the intro
 
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/.*intro/);
+});
+
+// This is a second example test, tagged only for the 'smoke' suite.
+test('Check for the search bar on the page @smoke', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  const searchButton = page.locator('.DocSearch-Button-Placeholder');
+  await expect(searchButton).toBeVisible();
+  await expect(searchButton).toHaveText('Search');
 });
